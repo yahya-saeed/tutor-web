e133f76fd47a97a1c4e94cc1588d91a74c83830e
yahya-saeed <yahyasaeed171@gmail.com>
2024-07-26 00:53:59 -0700

    javascript add

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ document.addEventListener('DOMContentLoaded', ()=> {
     })
 
 })
+
+// Change navbar background on scroll
+window.addEventListener('scroll', function () {
+    const navbar = document.querySelector('.navbar');
+  
+    if (window.scrollY > 0) {
+      navbar.classList.add('navbar-scroll');
+    } else {
+      navbar.classList.remove('navbar-scroll');
+    }
+  });
